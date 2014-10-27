eb7964244b4703f8e38eb90c767f8e7815ff3d31
Abdurrahman Köken <akoken@outlook.com>
2014-10-27 22:31:51 +0200

    textbox update property chaned behavior added

---
+++ Jirabox/Common/Behaviors/TextBoxUpdatePropertyChanged.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace Jirabox.Common.Behaviors
+{  
+    public class TextBoxUpdatePropertyChanged : Behavior<TextBox>
+    {
+        /// <summary>
+        /// Binding expression this behavior is attached to.
+        /// </summary>
+        private BindingExpression expression;
+
+        /// <summary>
+        /// Called after the behavior is attached to an AssociatedObject.
+        /// </summary>
+        /// <remarks>
+        /// Override this to hook up functionality to the AssociatedObject.
+        /// </remarks>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // Hook events to change behavior
+            expression = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+            AssociatedObject.TextChanged += OnTextChanged;
+        }
+
+        /// <summary>
+        /// Called when the behavior is being detached from its AssociatedObject, but before it has actually occurred.
+        /// </summary>
+        /// <remarks>
+        /// Override this to unhook functionality from the AssociatedObject.
+        /// </remarks>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            // Un-hook events
+            AssociatedObject.TextChanged -= OnTextChanged;
+            expression = null;
+        }
+
+        /// <summary>
+        /// Updates the source property when the text is changed.
+        /// </summary>
+        private void OnTextChanged(object sender, EventArgs args)
+        {
+            expression.UpdateSource();
+        }
+    }
+}
+
+
